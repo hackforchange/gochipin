c2026556cc32b91f2de2a4830a41907831e61ca4
Jeff Remer <jeff@threestarchina.com>
2011-06-18 12:26:34 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gochipin::Application.config.secret_token = 'facb537811f6c008cad49e51e0fc6a348619f039a1d13cee9f5f6616dce0c80ba2c12176395258e76a45d28df3200b7fef7cebb5507175bb115121c617611e46'
