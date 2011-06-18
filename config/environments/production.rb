5b844d0d9846c03ea46332567befc322e213ee88
Jeff Remer <jeff@threestarchina.com>
2011-06-18 13:06:00 -0700

    Added gochip.in as the actionmailer host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Gochipin::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'yourhost.com' }
+  config.action_mailer.default_url_options = { :host => 'gochip.in' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
